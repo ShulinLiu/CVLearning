dc29dbe0bf8944e4e3f4a334f871c73dc386b67e
ShulinLiu <shulin_liu@foxmail.com>
2018-04-18 21:27:28 +0800

    using edge detector to count coins in image

---
+++ counting_coins.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+image = cv2.GaussianBlur(image,(11,11),0)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+edged = cv2.Canny(image,30, 150)
+cv2.imshow("Edges", edged)
+cv2.waitKey(0)
+
+(_,cnts,_) = cv2.findContours(edged.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+print("I count {} coins in this image".format(len(cnts)))
+
+coins = image.copy()
+cv2.drawContours(coins,cnts,-1,(0,255,0),2)
+cv2.imshow("Coins",coins)
+cv2.waitKey(0)
+
+# for (1, c) in enumerate(cnts):
+# 	(x,y,w,h) = cv2.boundingRect(c)
+
+# 	print("Coins #{}".format(i + 1))
+# 	coin = image[y:y + h,x:x + w]
+# 	cv2.imshow("coin",coin)
